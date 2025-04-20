39c93313a701c67ba41e6bfb802993a72d89c30d
Hariharan Jayashankar <harijaya@hariharansair3.umd.edu>
2025-04-20 18:39:20 -0400

    vim slime works - with toggle term

--- lua/plugins/mainspec.lua
+++ lua/plugins/mainspec.lua
@@ -48,7 +48,27 @@ return {
     {('tzachar/local-highlight.nvim')},
     {
         'klafyvel/vim-slime-cells',
-        dependencies = {'jpalardy/vim-slime'}
+        dependencies = {'jpalardy/vim-slime'},
+        init = function()
+
+            vim.g.slime_target = "neovim"
+
+            -- disables default bindings
+            vim.g.slime_no_mappings = true
+        end,
+        config = function()
+            vim.g.slime_cell_delimiter = "^\\s*##"
+
+            -- send visual selection
+            vim.keymap.set("v", "<leader>s", '<Plug>SlimeRegionSend')
+            -- vim.keymap.set("n", "<leader>s", '<Plug>SlimeMotionSend')
+            vim.keymap.set("n", "<leader>as", '<Plug>SlimeLineSend')
+
+            -- SLime cells
+            vim.keymap.set("n", '<leader>s', '<Plug>SlimeCellsSendAndGoToNext')
+            vim.keymap.set("n", '<leader>j', '<Plug>SlimeCellsNext')
+            vim.keymap.set("n", '<leader>k', '<Plug>SlimeCellsPrev')
+        end
 
     },
      {
