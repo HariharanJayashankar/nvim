7028e61719431b3666b964a69915a60ef5300df5
Hariharan Jayashankar <harijaya@hariharansair3.umd.edu>
2025-04-20 23:36:23 -0400

    harpoon and oil had a conflict - fixeD

--- lua/harij/init.lua
+++ lua/harij/init.lua
@@ -1,6 +1,7 @@
 require("harij.lsp")
 require("harij.undotree")
 require("harij.set")
+require("harij.oil")
 require("harij.remap")
 require("harij.toggleterm")
 require("harij.slime")
--- lua/harij/remap.lua
+++ lua/harij/remap.lua
@@ -12,7 +12,7 @@ vim.keymap.set("n", "k", "gk")
 
 vim.keymap.set("t", "jk",  "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>vs", ":vs<CR>")
-vim.keymap.set("n", "<leader>s", ":split<CR>")
+vim.keymap.set("n", "<leader>hs", ":split<CR>")
 
 vim.keymap.set("n", "gb", ":buffers<CR>:buffer<Space>")
 vim.keymap.set("n", "<Tab>", ":bn<CR>")
